50388594ad0da95227b435048ef207b038a94819
Afreenkhan <ak2712463@gmail.com>
2020-06-15 10:38:58 -0700

    prac

---
+++ app.js
@@ -0,0 +1,425 @@
+// CHAPTER 1 
+//QUESTION 1
+var hey = "Hello , Hope you doing well, Welcome to the Website";
+alert(hey);
+
+//QUESTION 2
+var err = "Error! Please Enter A valid password";
+alert(err);
+
+//QUESTION 3
+ 
+alert( "Welcome to JS LAND \n Happy Coding!");
+
+//QUESTION 4
+
+alert("Welcome to JS LAND");
+alert("Happy Coding");
+
+// QUESTION 5
+var alp = "Hello , I can Run Js console too";
+console.log(alp);
+
+//CHAPTER 2 VARIABLES WITH STRING
+//QUESTION 1
+
+var username;
+
+//QUESTION 2
+var myName = "Afreen Khan";
+
+//QUESTION 3
+var message = "Hello World";
+alert(message);
+
+//QUESTION 4
+var name= "Jhone due";
+var age = "15 years old";
+var spec = "Mobile applications";
+alert(name);
+alert(age);
+alert(spec);
+
+//QUETION 5
+var a = ("PIZZA \n PIZZ \n PIZ \n PI \n P" );
+alert(a);
+
+//QUESTION 6
+var email = "ak2712463@gmail.com";
+alert ("My Email Addres Is" + email);
+
+//QUESTION 7
+var book = "A Smarter way to Learn Javascript";
+alert("I M trying to Learn Javascript from " + "" + book);
+
+//QUESTION 8
+var cont = "Yayyy I can write Js through HTML too";
+document.write(cont);
+
+//QUESTION 9 
+var des = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(des);
+
+//CHAPTER 3
+//QUESTION 1
+var age = 22;
+alert("MY AGE IS " + age);
+
+//QUESTION 2
+var n = 16
+alert("You have visited" + n + "times");
+
+//QUESTION 3
+var birthYear = 1998;
+document.write("My BirthYear is " + birthYear + "<br>" + "Data Type of my variable is a number");
+
+//QUESTION 4
+var namee = "Afreen Khan";
+var prod = "Earpods";
+var quan = 6;
+document.write(name + "" + "Ordered" + "" + quan + "" + prod + ""+ "On Daraz store");
+ 
+//CHAPTER 5
+//QUESTION 1 & 2
+var num1 = 122;
+var num2 =228;
+var newnum = num1 + num2;
+document.write("The sum of 132 and 228 is " + "" + newnum + "<br>");
+newnum = num1-num2;
+document.write("The sum of 132 and 228 is " + "" + newnum + "<br>");
+newnum = num1* num2;
+document.write("The sum of 132 and 228 is " + "" + newnum + "<br>")
+newnum = num1%num2;
+document.write("The sum of 132 and 228 is " + "" + newnum + "<br>")
+
+//QUESTION 3
+
+var any ;
+document.write("The value of Variable after declaring is " + any + "<br>" );
+any = 10;
+document.write("Initial Value is " + any + "<br>");
+any = any++;
+document.write("Value after increment is " + any + "<br>");
+any = any+7;
+document.write("Value after Adding 7 to it " + any + "<br>");
+any = any--;
+document.write("Value after Decrement is " + any + "<br>" );
+any = any%3;
+document.write("Value Remainder is " + any + "<br>");
+
+//QUESTION 5
+var movTik = 600;
+movTik= movTik*5;
+document.write("The Sum of 5 tickets is " + movTik + "<br>");
+
+//QUESTION 6
+var cel = 250;
+var f  = 540;
+cel= (f-32) * 5/9;
+f = (cel *9/5) +32;
+document.write("250 c is " + f + "ferhenhits");
+document.write("540 f is " + cel + "celcius");
+
+//QUESTION 7
+var pri = 300;
+var pri2 = 500;
+var ans1= 300*3;
+var ans2 = 500*6;
+var ord1 =3;
+var ord2 =6;
+var shipCharge = 200;
+var total = ans1+ ans2;
+document.write("Price of Item 1 is " + pri + "<br>");
+document.write("Quantity of Item 1 is " + ord1 +" <br>");
+document.write("Price of Item 2 is " + pri2 + "<br>");
+document.write("Quantity of Item 2 is " + ord2 + "<br>");
+document.write("Shipping Fee is " + shipCharge + "<br>");
+document.write("Total is : " + total + "<br>");
+ 
+//QUESTION 8
+
+document.write("<br>" + "<br>" + "MARKSHEET");
+var tot = 980;
+var marksobt = 850;
+var percent = marksobt/tot *100;
+document.write("Percentage is : " + percent);
+
+//QUESTION 9
+
+var us = 104.80* 10;
+var saudi = 28 *25;
+var totali = us + saudi;
+document.write("The Total of 10 dollar and 25 saudi riyal is equal to " + totali + "In Pakistanii Rupees" + "<br>");
+
+//QUESTION 10
+
+var numi = 10;
+numi= numi+ 5;
+document.write("Addition of 10 and 5 results  " + numi + "<br>");
+numi = numi *10;
+document.write("Multiplication of 10 and 10 is " +numi + "<br>");
+numi =numi/2;
+document.write("Division results in " + numi + "<br>");
+
+//QUESTION 11
+
+var current = 2020;
+var birth = 1990;
+var age2 = 1990-2020;
+document.write("Your Current age is " + age2 + "<br>");
+
+//QUESTION 12
+
+var rad = 45;
+var pie = 3.142;
+var cir = 2*pie*rad;
+document.write("The radius of circle is " + rad + "<br>");
+document.write("Circumferrance of a Circle is " + cir +"<br>");
+var area = pie* rad*rad ;
+document.write("The area of circle is " + area);
+
+//QUESTION 13
+
+var favsnack = "Lays";
+var currentAge = 20;
+var maxiAge = 50;
+var estiAmont = 50;
+var kuch =50*50;
+document.write("you will need " + kuch + "To Eat " + favsnack + "Till the Age of" + maxiAge + "<br>");
+
+//CHAPTER 6 -9
+//QUESTION 1
+var a= 10;
+a = ++a;
+document.write("The Value afterr ++a is " + a +" <br>");
+document.write("Now the value of a is "  + a + "<br>");
+a = a++;
+document.write("The value of a++ is " + a + "<br>");
+a= a++;
+document.write("Now the Value is " + a + "<br>");
+a= --a;
+document.write("The value of --a is " + a + "<br>");
+document.write("Now the Value is " + a + "<br>");
+a= a--;
+document.write("The value of --a is " + a + "<br>");
+document.write("Now the Value is " + a + "<br>");
+
+//QUESTION 3
+ var yh = prompt("Enter your name ");
+ document.write("This Websit says hello to you Mr/ Mrs " + yh);
+
+ //QUESTION 5
+//  var tab = prompt("Enter Any Number for multiplcation table");
+//  document.write(tab)
+
+
+ //QUESTION 6
+
+ var maths = prompt("Enter first Subject");
+ var enG = prompt("Enter the Second Subject");
+var comp = prompt("Enter Third Subject");
+var totalmarks = 100;
+var m1 = prompt("Enter the Marks Obtained in " + maths + "<br>");
+var m2 = prompt("Enter the Marks Obtained in " + enG + "<br>");
+var m3 = prompt("Enter the Marks Obtained in "+ comp + "<br>");
+var per1 = m1 /totalmarks*100;
+var per2 = m2/totalmarks*100;
+var per3 = m3 /totalmarks*100;
+document.write("Marks obtained : " + maths + "=" + m1 + " percentage : " + per1 + "<br>");
+document.write("Marks obtained : " + enG + "=" + m2 + " percentage : " + per2  + "<br>");
+document.write("Marks obtained : " + comp + "=" + m3 + " percentage : " + per3 + "<br>");
+
+//CHAPTER 9 - 11
+// QUESTION 1
+
+var cityName = prompt("Enter your City Name");
+if(cityName === 'Karachi'){
+    alert("Welcome to the City Of Lights")
+}
+
+//QUESTION 2
+
+var gend = prompt("Please Enter your gender");
+if(gend === 'Male')
+{
+    alert("Good morning Sir")
+}
+else if(gend === 'Female'){
+    alert("Good Morning Maa'am ")
+
+}
+
+//QUESTION 3
+var col = prompt("Enter Any Color from RED, YELLOW GREEN");
+if(col=== 'Red')
+{
+    alert("You have to stop")
+}
+else if(col === 'Yellow')
+{
+    alert("Get Ready")
+}
+else if (col === 'Green')
+{
+alert(    "You Can Go Now")
+}
+
+//QUESTION 4
+
+var fuel = prompt("Enter Your Fuel in liters");
+if(fuel < 1.245){
+    alert("You need to fill it soon")
+}
+else if(fuel >1.245)
+{
+    alert("You hve enogh Fuel")
+}
+
+//QUESTION 6
+var ma1=prompt("Ener the Marks of First Subject");
+var ma2 = prompt("Enter the marks of Second Subject");
+ var ma3 = prompt("Enter the Marks of Third Subject");
+ var max = 150;
+ var alltot = 450;
+ var percent = ma1 + ma2+ ma3/alltot*100;
+ if(percent >= 80)
+ {
+     document.write("A+ grade" + "<br>");
+     document.write("Remarks : Excellent!");
+ }
+ else if (percent >= 70){
+    document.write("A" + "<br>");
+    document.write("Remarks : Good!" + "<br>") ;
+ }
+ else if (percent >= 60){
+    document.write("B" + "<br>");
+    document.write("Remarks :  You Need To Improve!" + "<br>") ;
+ }
+ else if (percent < 60){
+    document.write("F" + "<br>");
+    document.write("Remarks :  Sorry you failed!" + "<br>") ;
+ }
+
+ //QUESTION 7
+ var secNum = 8;
+ var anss=  prompt("Guess The Number Between 1 - 10");
+ if (anss === 8)
+ {
+     alert("yu guess it right!");   }
+     else if(anss > 2 && anss < 6){
+         alert("You Aare Near, think MOre");
+     }
+
+     //QUESTION 8
+
+     var divi = prompt("Enter any number to know whether its divisible by 3 or not");
+     if(divi%3==0){
+         alert("The Number is divisible by 3");
+     }
+     else{
+         alert("Number is not divisible by 3");
+     }
+
+     //QUESTION 9
+     var odd = prompt("Enter any number To find out its Even or odd");
+     if(odd %2 == 0)
+     {
+    alert("Number entered is Even");_
+     }
+     else {
+         alert("Number Entered Is Odd");
+     }
+
+     // QUESTION 9
+
+     var temp = prompt("Enter Temperature");
+     if(temp> 40){
+         alert("Its Too Hot Outside");
+     }
+  else if (temp >30)
+  {
+      alert("Whether is Normall outside");
+  }
+  else if(temp >20)
+  {
+      alert("Today weather is cool")
+  }
+  else if(temp >10)
+  {
+      alert("OMG! the weather is soo cool");
+  }
+
+  //CHPTERS 11 - 13
+//QUESTION 1
+var u = prompt("Enter any Letter to find out its in upper ase or lower case");
+if(u >= 65 && u <= 90)
+{
+    alert("Its in Upper case");
+}
+else if(u >=97 || u <=127 )
+{
+    alert("Its in Lower case");
+}
+else{
+    alert("Its A number");
+}
+
+//QUESTION 2
+var num1 = 228;
+var num2= 130;
+if(num1>num2){
+    alert("Number 1 is greater i.e" + num1)
+}
+else if(num1 < num2)
+{
+    alert("Number 2 is greater i.e "+ num2 )
+}
+else if(num1 ==num2){
+    alert("Both are equal")
+}
+
+//QUESTION 3
+
+var poi = prompt("Enter Any Number to know whether its A positive number or negitive");
+if(poi > 1){
+    alert("Number is Positive");
+}
+
+else if(poi < 1){
+    alert("Number is Negitive");
+}
+
+//QUESTION 6
+//the synatx after the if else statement is not correct
+
+//QUESTION 5
+var pass= "shinninggirl";
+var enter = prompt("Enter the password");
+if(enter === 'shinninggirl')
+{
+    alert("Correct! The password you entered matches the original password")
+}else{
+    alert("INVALID! PASSWORD")
+}
+
+//ChHAPTER 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
